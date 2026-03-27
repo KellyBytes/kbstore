c2c12c50ff7ccea2fe290b90049da72b280c3d09
Kelly <kellybytes.ca@gmail.com>
2026-03-26 20:21:33 -0600

    Add isFeatured & banner

--- lib/validators.ts
+++ lib/validators.ts
@@ -19,8 +19,8 @@ export const insertProductSchema = z.object({
   // stock: z.coerce.number(),
   stock: z.number().int().min(0),
   images: z.array(z.string()).min(1, 'Product must have at least one image'),
-  // isFeatured: z.boolean(),
-  // banner: z.string().nullable(),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
   price: currency,
 });
 
