895f45bf6246479ea81a341a1cdbdff826e0e110
eiwashita <erikoiwashita@gmail.com>
2026-06-22 16:18:53 -0600

    Update deal end date to 2026-08-01

--- components/deal-countdown.tsx
+++ components/deal-countdown.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 // Static target date (replace with desired date)
-const TARGET_DATE = new Date('2026-06-04T00:00:00');
+const TARGET_DATE = new Date('2026-08-01T00:00:00');
 
 // Function to calculate the time remaining
 const calculateTimeRemaining = (targetDate: Date) => {
